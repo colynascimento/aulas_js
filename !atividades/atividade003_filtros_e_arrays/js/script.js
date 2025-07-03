ef5cee76a5bbb8b17963deea61e8f521d125683b
colynascimento <colymsnascimento@gmail.com>
2025-07-02 21:42:13 -0300

    atividade003 função criarCards

--- !atividades/atividade003_filtros_e_arrays/js/script.js
+++ !atividades/atividade003_filtros_e_arrays/js/script.js
@@ -1,21 +1,41 @@
-const cardsProdutos = document.querySelectorAll('.card-produto');
 const listaProdutos = [
-    {nome: 'Sabonete Natural', preco: 15.0, categoria: 'cosmetico'},
-    {nome: 'Shampoo Orgânico', preco: 25.0, categoria: 'cosmetico'},
-    {nome: 'Granola Artesanal', preco: 18.5, categoria: 'alimento'},
-    {nome: 'Mel Puro', preco: 22.0, categoria: 'alimento'}
+    {nome: 'Sabonete Natural', preco: 15.0, categoria: 'cosmeticos'},
+    {nome: 'Shampoo Orgânico', preco: 25.0, categoria: 'cosmeticos'},
+    {nome: 'Granola Artesanal', preco: 18.5, categoria: 'alimentos'},
+    {nome: 'Mel Puro', preco: 22.0, categoria: 'alimentos'}
 ];
-const elementoCategoria = document.getElementById('lista-categorias');
-const categoriaSelecionada = elementoCategoria.value;
 
-function filtrarProdutos(){
-    listaProdutos.forEach(produto => {
-        if (!produto.classList.contains(categoriaSelecionada)) {
-            produto.style.display = 'none';
-        };
-    });
+const containerProdutos = document.querySelector('#container-produtos');
+
+function criarCards(){
+    for(let i = 0; i < listaProdutos.length; i++) {
+        const cardProduto = document.createElement('div');
+        cardProduto.className = `card-produto ${listaProdutos[i].categoria}`;
+        const tituloProduto = document.createElement('h4'); 
+        tituloProduto.className = 'nome-produto';
+        const precoProduto = document.createElement('p');
+        precoProduto.className = 'preco-produto';
+
+        containerProdutos.appendChild(cardProduto);
+        cardProduto.appendChild(tituloProduto);
+        cardProduto.appendChild(precoProduto);
+
+        tituloProduto.innerText = listaProdutos[i].nome;
+        precoProduto.innerText = listaProdutos[i].preco;
+    };
 };
 
-// montar a lista de produtos dinamicamente
+// const elementoCategoria = document.getElementById('lista-categorias');
+// const categoriaSelecionada = elementoCategoria.value;
 
-document.getElementById('lista-categorias').addEventListener('change', filtrarProdutos());
+// function filtrarProdutos(){
+//     listaProdutos.forEach(produto => {
+//         if (!produto.classList.contains(categoriaSelecionada)) {
+//             produto.style.display = 'none';
+//         };
+//     });
+// };
+
+// // montar a lista de produtos dinamicamente
+
+// document.getElementById('lista-categorias').addEventListener('change', filtrarProdutos());
